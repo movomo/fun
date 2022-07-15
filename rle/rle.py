21e7fb716c90590a6a742eacc275bd8bb0c0c784
movomo <movomo@gmail.com>
2022-07-15 23:16:28 +0900

    module outline

---
+++ rle/rle.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+import io
+
+
+def compress(data):
+    """Compress data with simple rle."""
+
+
+def decompress(data):
+    """Decompress rle-compressed data to original."""
+
+
+if __name__ == '__main__':
+    ...
